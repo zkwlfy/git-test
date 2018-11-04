4c08f9224d42e3cc164782cd23b4846f29ea9caf
zhangzhangzhang <1374896296@qq.com>
2018-11-04 21:40:53 +0800

    添加

---
+++ git-test-project/src/com/ithei/git/First.java
@@ -0,0 +1,15 @@
+package com.ithei.git;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+    }
+    public void test(){
+        int i=0;
+        i +=10;
+    }
+}
